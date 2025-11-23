9b87844f7b47b11e35fb0a7c0481d33fc3b19a31
Your Name <you@example.com>
2025-11-23 22:18:39 +0100

    fix requirements

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+import  reviews.urls 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/reviews/', include('reviews.urls')),  # <-- link app URLs
+    path('api/reviews/', include("reviews.urls")),  # <-- link app URLs
     path('api/', include('social.urls')),
     path('bucket/', include('bucket.urls')),
 ]
