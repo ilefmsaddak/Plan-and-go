bd683b0157ac627d58e9ef5f1e36ecc81e790610
Ilef Msaddak <ilef.msaddak@etudiant-fst.utm.tn>
2026-02-10 15:43:44 +0100

    update

--- frontend/src/app/environment/env.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  openRouteServiceKey:'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjIwMzBiMGNiZjczNjRlMDFiOWY3NGMzNTFmY2ZmZmNhIiwiaCI6Im11cm11cjY0In0=', // OpenRouteService key for routing
-  apiBaseUrl: 'http://localhost:8000/api',
-  mapProxyUrl: 'http://localhost:8000/api/map/proxy/serpapi/'
-};
